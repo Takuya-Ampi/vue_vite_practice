42ac380dcae8fc0266c6f1950acb348d810778e8
Takuya-Ampi <ap.takuya@gmail.com>
2021-09-21 15:14:17 +0900

    fix: reactive

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,4 +1,18 @@
 <template>
   <h1>About!!</h1>
   <router-link to="/">Go to home</router-link>
+  <p>数字</p>
+  <span>{{ val }}</span>
 </template>
+
+<script lang="ts">
+  import { defineComponent, ref } from "vue";
+
+  export default defineComponent({
+    setup: () => {
+
+      const val = ref(0);
+      return { val };
+    },
+  });
+</script>
